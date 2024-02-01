6bf5bbd92e6ddad389bef6626311fc3d350a2ad8
KJSoo95 <wltn7star@naver.com>
2024-02-02 04:50:36 +0900

    feat : navi컴포넌트 이벤트 추가

--- Portfolio-vite/src/componenet/Section/Navigation/Navi.jsx
+++ Portfolio-vite/src/componenet/Section/Navigation/Navi.jsx
@@ -1,10 +1,22 @@
 import classes from "./Navi.module.css";
 
-const Navi = () => {
+const Navi = (props) => {
+  const GotoTop = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
+
   return (
     <div className={classes.navi}>
-      <div className={classes.left}>Kang ji su</div>
-      <div className={classes.right}>Front-End Developer</div>
+      <div className={classes.left} onClick={GotoTop}>
+        Kang ji su
+      </div>
+      <div onClick={props.onAboutClick}>About</div>
+      <div onClick={props.onProjectClick}>Project</div>
+      <div onClick={props.onSkillClick}>Skill</div>
+      <div onClick={props.onContactClick}>Contact</div>
+      <div className={classes.right} onClick={GotoTop}>
+        Front-End Developer
+      </div>
     </div>
   );
 };
