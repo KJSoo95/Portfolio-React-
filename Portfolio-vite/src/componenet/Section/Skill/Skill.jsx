bd8f533a6a29a49f8474d798827da5299497b96b
KJSoo95 <wltn7star@naver.com>
2024-02-28 23:44:53 +0900

    feat : 스킬 부분 내용 추가

--- Portfolio-vite/src/componenet/Section/Skill/Skill.jsx
+++ Portfolio-vite/src/componenet/Section/Skill/Skill.jsx
@@ -12,27 +12,29 @@ import fireImg from "../../../images/firebase.jpg";
 
 const Html = {
   skill: "HTML",
-  description: "",
+  description:
+    "시멘틱 태그로 구조 설계 하여 좀 더 명시적이며 구조적인 설계가 가능함.",
 };
 const Css = {
   skill: "CSS",
-  description: "",
+  description: "모듈 CSS를 사용 가능하며 CSS로 애니메이션 구현도 가능함.",
 };
 const Javascript = {
   skill: "Javascript",
-  description: "",
+  description:
+    "기본 자바스크립트 언어 사용이 가능하며 기본적인 함수 사용 가능함. ES6문법을사용.",
 };
 const Jquery = {
   skill: "jQuery",
-  description: "",
+  description: "jQuery 사용해본 경험이 있음. 어떻게 사용해야하는지 알고있음.",
 };
 const React = {
   skill: "React",
-  description: "",
+  description: "컴포넌트 분리, 상태관리, 리액트 훅 등 사용 가능함.",
 };
 const Firebase = {
   skill: "Firebase",
-  description: "",
+  description: "빌드와 배포 사용한 경험이 있음.",
 };
 
 const Skill = forwardRef((props, ref) => {
